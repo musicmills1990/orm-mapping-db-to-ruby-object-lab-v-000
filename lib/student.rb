b791a50f35e005444b232f1b38f8497726cab5bc
Marc Mills <mdmills1990@gmail.com>
2018-10-25 17:42:47 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -50,7 +50,15 @@ class Student
     end
   end
 
-  def self.first_X_students_in_grade_10
+  def self.first_X_students_in_grade_10(num)
+    sql = <<-SQL
+    SELECT * FROM students
+    WHERE grade = 10 LIMIT ?
+    SQL
+    DB[:conn].execute(sql, num).map do |row|
+      self.new_from_db(row)
+    end
+
   end
 
   def self.first_student_in_grade_10
