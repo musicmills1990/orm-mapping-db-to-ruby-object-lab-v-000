f504d903b0cd4e750df14aedd74d400d453ddb84
Marc Mills <mdmills1990@gmail.com>
2018-10-25 17:38:49 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,7 +35,7 @@ class Student
     SELECT * FROM students
     WHERE grade = 9
     SQL
-    DB[:conn].execute(sql, name).map do |row|
+    DB[:conn].execute(sql, grade).map do |row|
       self.new_from_db(row)
     end
   end
